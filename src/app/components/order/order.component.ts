f03364a455e5320fb64aadfb829f93afb1b49365
Gerardo Gutierrez <geragutierrezsalta@gmail.com>
2022-05-15 00:26:48 -0300

    formarray complete

--- src/app/components/order/order.component.ts
+++ src/app/components/order/order.component.ts
@@ -2,24 +2,39 @@ import { Component, Input, OnInit } from '@angular/core';
 import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
-  selector: 'app-order',
-  templateUrl: './order.component.html',
-  styleUrls: ['./order.component.scss']
+	selector: 'app-order',
+	templateUrl: './order.component.html',
+	styleUrls: ['./order.component.scss']
 })
 export class OrderComponent implements OnInit {
 
-  orderType: any = 'vinyl'
-  description: boolean = false
+	form: FormGroup = new FormGroup({
+		arrOrders: new FormArray([])
+	});
+	orders: FormArray = this.form.get('arrOrders') as FormArray;
+	description: boolean = false;
 
-  constructor() { }
+	constructor() { }
 
-  ngOnInit(): void {
-  }
+	ngOnInit(): void {
+		this.addOrder()
+	}
 
-  openDescription(){
-    this.description === false ? this.description = true : this.description = false
-  }
-  
-  addOrder(){
-  }
+	openDescription() {
+		this.description === false ? this.description = true : this.description = false
+	}
+
+	addOrder() {
+		let newOrder = new FormGroup({
+			material: new FormControl('vinyl'),
+			plotter: new FormControl(''),
+			type: new FormControl(''),
+			height: new FormControl(''),
+			width: new FormControl(''),
+			additionalMaterial: new FormControl(''),
+			additionalService: new FormControl('')
+		});
+		this.orders?.controls.push(newOrder);
+	}
+
 }
