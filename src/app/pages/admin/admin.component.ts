71754756db2cb5d9eb3abf72cd67399b134ba8ff
gerasalta <geragutierrezsalta@gmail.com>
2022-07-03 14:57:00 -0300

    get request prepared

--- src/app/pages/admin/admin.component.ts
+++ src/app/pages/admin/admin.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
 import { DatabaseService } from 'src/app/services/database.service';
 
 @Component({
@@ -11,7 +12,10 @@ export class AdminComponent implements OnInit {
   page: number = 1;
   prevPage: boolean = false;
   nextPage: boolean = false;
-  updatePack: any = {};
+  material: FormControl = new FormControl('vinyl');
+  service: FormControl = new FormControl('print');
+  newPrice: FormControl = new FormControl(0 );
+  updatePack: string = "";
 
   constructor(private db: DatabaseService) { }
 
@@ -35,6 +39,15 @@ export class AdminComponent implements OnInit {
   disabledPagintion(){
     this.page === 1 ? this.prevPage = true : this.prevPage = false;
     this.page === 3 ? this.nextPage = true : this.nextPage = false;
+  }
+
+  updatePrice(){
+   this.updatePack = `{${this.service.value}:${this.newPrice.value}}`;
+    this.db.updatePrice(this.material.value, this.service.value, this.updatePack)
+    .subscribe({
+      next: data => console.log(data),
+      error: err => console.log(err)      
+    })
   }
 
 }
--- src/app/services/database.service.ts
+++ src/app/services/database.service.ts
@@ -29,4 +29,8 @@ export class DatabaseService {
     return this.http.delete(`${this.apiUrl}/orders/${id}`)
   }
 
+  updatePrice(material: string, service: string, updatePack: string){
+    return this.http.put(`${this.apiUrl}/prices?material=${material}&service=${service}`, updatePack)
+  }
+
 }
